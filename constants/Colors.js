771c0d19588f6e1616feac072168f310bbf9c5e8
Amrit <amritpalsingha4@gmail.com>
2019-08-19 18:17:47 -0400

    boilerplate expo starter

---
+++ constants/Colors.js
@@ -0,0 +1,16 @@
+const tintColor = '#2f95dc';
+
+const black = '#181818';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: black,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
